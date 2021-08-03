a12d8c1bd0df73d8b7735158281e0a6bd1131b9a
MCSuda <suder.michal97@gmail.com>
2021-08-03 12:27:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+
+if __name__ == '__main__':
+    with open('birthdays.json', 'r') as f:
+        data = json.load(f)
+
+    for i in data:
+        y = i
+        print(i + ": " + data[y])
+
+    name = input("Give me new name: ")
+    year = input("Give me year of birth of that person: ")
+    entry = {name: year}
+    data.update(entry)
+
+    with open('birthdays.json', 'w')as f:
+        json.dump(data, f)
